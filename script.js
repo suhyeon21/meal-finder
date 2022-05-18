206d3a387e9991796ede474fe8adafa221460a87
suhyeon <suhyeon.kan@gmail.com>
2022-05-18 23:47:28 +0900

    feat: Create random meal

--- script.js
+++ script.js
@@ -58,6 +58,21 @@ function getMealById(mealID) {
     });
 }
 
+//Fetch random meal from API
+function getRandomMeal() {
+  //Clear meals and heading
+  mealsEl.innerHTML = "";
+  resultHeading.innerHTML = "";
+
+  fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+
+      addMealToDOM(meal);
+    });
+}
+
 //Add meal to DOM
 
 function addMealToDOM(meal) {
@@ -80,7 +95,7 @@ function addMealToDOM(meal) {
         ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ""}
         ${meal.strArea ? `<p>${meal.strArea}</p>` : ""}
       </div>
-      
+
       <div class="main">
         <p>${meal.strInstructions}</p>
         <h2>Ingredients</h2>
@@ -93,6 +108,7 @@ function addMealToDOM(meal) {
 }
 //Event listener
 submit.addEventListener("submit", searchMeal);
+random.addEventListener("click", getRandomMeal);
 
 mealsEl.addEventListener("click", (e) => {
   const mealInfo = e.path.find((item) => {
